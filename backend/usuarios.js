5dc0cd6dcb7a03f19de59e0390bb6947cfeae6f6
Salas Cabral Juan Manuel <juanmanuelcabralll@gmail.com>
2024-11-07 19:40:34 -0300

    Agregar validación campos vacios

--- backend/usuarios.js
+++ backend/usuarios.js
@@ -11,6 +11,12 @@ router.get("/", (req, res) => {
 });
 
 //Crear usuario nuevo
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { nombre, apellido, nombre_usuario, contraseña } = req.body;
+  if (!nombre || !apellido || !nombre_usuario || !contraseña) {
+    res.status(400).json({ error: "Datos incompletos" });
+    return;
+  }
+});
 
 export default router;

7181f2e8f7552b3e314a3bc060aafd6ef627db32
Salas Cabral Juan Manuel <juanmanuelcabralll@gmail.com>
2024-11-07 19:20:42 -0300

    Endpoint Crear usuario

--- backend/usuarios.js
+++ backend/usuarios.js
@@ -3,10 +3,14 @@ import express from "express";
 const router = express.Router();
 
 let usuarios = [];
+let id = 0;
 
 //Obtenemos todos los usuarios
 router.get("/", (req, res) => {
   res.send(usuarios);
 });
+
+//Crear usuario nuevo
+router.post("/", (req, res) => {});
 
 export default router;

b4a6d9569d073dd373e953cf19a7a459bbf4533e
Salas Cabral Juan Manuel <juanmanuelcabralll@gmail.com>
2024-11-10 21:01:42 -0300

    Agregar enrutador y uso de enrutador

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import routerUsuarios from "./usuarios.js";
+import routerClientes from "./clients.js";
 
 const app = express();
 const PORT = 3000;
@@ -13,6 +14,9 @@ app.get("/", (req, res) => {
 
 //Uso de enrutador de Usuario
 app.use("/app/usuarios", routerUsuarios);
+
+// Uso de enrutador de clientes
+app.use("/api/clientes", routerClientes);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);

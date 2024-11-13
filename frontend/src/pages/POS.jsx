f866e4e69c9aebee7c766032c70d3eb323b90e70
Xander.- <zn.xander@gmail.com>
2024-11-13 18:46:42 -0300

    Mejorar estilo del POS

--- frontend/src/pages/POS.jsx
+++ frontend/src/pages/POS.jsx
@@ -66,90 +66,107 @@ const POS = () => {
   ]
 
   return (
-    <div className="flex flex-row justify-center items-start gap-x-3">
-      <div className="basis-3/4">
-        <div className="flex justify-between">
-          <button>+ AGREGAR PRODUCTO NUEVO</button>
-          <div className="flex justify-end">
-            <input type="text" placeholder="Buscar producto..." />
-            <a href="">🔎</a>
+    <div className="flex flex-col min-h-dvh">
+      <header className="h-12 shrink-0">
+        <h1 className="text-3xl font-bold">POS</h1>
+      </header>
+      <main className="flex flex-row justify-center gap-x-3 flex-grow px-8">
+        <div className="basis-3/4">
+          <div className="flex justify-between">
+            <button>+ AGREGAR PRODUCTO NUEVO</button>
+            <div className="flex justify-end">
+              <input type="text" placeholder="Buscar producto..." />
+              <a href="">🔎</a>
+            </div>
+          </div>
+          <div className="flex flex-wrap overflow-y-scroll h-[calc(100vh-11rem)]">
+            {productos.map((producto, index) => (
+              <button
+                className="flex flex-col basis-1/4 bg-blue-950 border rounded-lg p-3 justify-center items-center text-white"
+                key={index}>
+                <img
+                  src="https://www.shutterstock.com/shutterstock/photos/2232098881/display_1500/stock-photo-different-fresh-vegetables-for-eating-healthy-fresh-vegetables-in-basket-isolated-on-white-2232098881.jpg"
+                  alt=""
+                />
+                <h2>{producto.nombre}</h2>
+                <p>${producto.precio.toFixed(2)}</p>
+              </button>
+            ))}
+          </div>
+
+          <div className="flex h-12 ">
+            {categorias.map((categoria, index) => (
+              <button
+                className="basis-1/5 flex flex-col bg-blue-950 border rounded-lg p-3 m-1 justify-center items-center text-white"
+                key={index}>
+                {categoria.nombre}
+              </button>
+            ))}
           </div>
         </div>
-        <div className="grid grid-cols-4 overflow-y-scroll h-[70vh] ">
-          {productos.map((producto, index) => (
-            <button
-              className="flex flex-col bg-blue-950 border rounded-lg p-3 m-1 justify-center items-center text-white"
-              key={index}>
-              <img
-                src="https://www.shutterstock.com/shutterstock/photos/2232098881/display_1500/stock-photo-different-fresh-vegetables-for-eating-healthy-fresh-vegetables-in-basket-isolated-on-white-2232098881.jpg"
-                alt=""
-              />
-              <h2>{producto.nombre}</h2>
-              <p>${producto.precio.toFixed(2)}</p>
-            </button>
-          ))}
-        </div>
-
-        <div className="grid grid-cols-5 h-[20vh] ">
-          {categorias.map((categoria, index) => (
-            <button
-              className="flex flex-col bg-blue-950 border rounded-lg p-3 m-1 justify-center items-center text-white"
-              key={index}>
-              {categoria.nombre}
-            </button>
-          ))}
-        </div>
-      </div>
-      <div className="basis-1/4 flex flex-col m-auto">
-        <h2>Checkout</h2>
-        <table className="table-fixed border">
-          <thead>
-            <tr>
-              <th>Producto</th>
-              <th>Cantidad</th>
-              <th>Precio</th>
-            </tr>
-            {productos.map((producto, index) => (
-              <tr key={index}>
-                <td>{producto.nombre}</td>
-                <td>
-                  <span>-</span> {producto.cantidad} <span>+</span>
-                </td>
-                <td>${producto.precio.toFixed(2)}</td>
-              </tr>
-            ))}
-          </thead>
-        </table>
-        <ul>
-          <li className="flex justify-between">
-            <span>Descuento:</span>
-            <span>
-              $
-              <input
-                className="text-right w-20"
-                type="number"
-                placeholder="0.00"></input>
-            </span>
-          </li>
-          <li className="flex justify-between">
-            <span>Subtotal:</span>
-            <span>$0.00</span>
-          </li>
-          {/* <li>
+        <div className="basis-1/4 flex flex-col flex-grow justify-between">
+          <div className="flex flex-col">
+            <h2>Checkout</h2>
+            <table className="table-fixed border">
+              <thead>
+                <tr>
+                  <th>Producto</th>
+                  <th>Cantidad</th>
+                  <th>Precio</th>
+                </tr>
+                {productos.map((producto, index) => (
+                  <tr key={index}>
+                    <td>{producto.nombre}</td>
+                    <td>
+                      <span>-</span> {producto.cantidad} <span>+</span>
+                    </td>
+                    <td>${producto.precio.toFixed(2)}</td>
+                  </tr>
+                ))}
+              </thead>
+            </table>
+          </div>
+          <ul>
+            <li className="flex justify-between">
+              <span>Descuento:</span>
+              <span>
+                $
+                <input
+                  className="text-right w-20"
+                  type="number"
+                  placeholder="0.00"></input>
+              </span>
+            </li>
+            <li className="flex justify-between">
+              <span>Subtotal:</span>
+              <span>$0.00</span>
+            </li>
+            {/* <li>
             <span>Impuestos:</span>
             <span>$0.00</span>
           </li> */}
-          <li className="flex justify-between">
-            <span>Total:</span>
-            <span>$0.00</span>
-          </li>
-        </ul>
-        <div className="flex w-full">
-          <button className="bg-blue-950 text-white p-2 rounded-lg w-full">
-            Realizar pago
+            <li className="flex justify-between">
+              <span>Total:</span>
+              <span>$0.00</span>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer className="h-12 flex flex-shrink-0 gap-x-3 px-8">
+        <div className="basis-3/4 flex justify-end gap-x-3">
+          <button className="px-8 text-blue-950-950 bg-red-800 border rounded-lg text-white h-full">
+            Cancelar venta
+          </button>
+          <button className="px-8 text-blue-950-950 bg-blue-950 border rounded-lg text-white h-full">
+            Mantener venta
           </button>
         </div>
-      </div>
+        <div className="basis-1/4">
+          <button className="px-8 text-blue-950-950 bg-blue-950 border rounded-lg text-white w-full h-full">
+            Finalizar venta
+          </button>
+        </div>
+      </footer>
     </div>
   )
 }

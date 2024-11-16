e2a819b063d289b38a8541b780f3350571c219e4
Xander.- <zn.xander@gmail.com>
2024-11-16 15:48:30 -0300

    Username y Email son unicos para cada usuario

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,10 +3,14 @@ import mongoose from "mongoose"
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   lastName: { type: String, required: true },
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
-  role_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Role', required: true },
+  email: { type: String, required: true, unique: true },
+  role_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Role",
+    required: true
+  },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date }
 })

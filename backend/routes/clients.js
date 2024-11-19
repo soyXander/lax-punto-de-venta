b4ee19f36ebd036cf496074092efe1a4065de716
Salas Cabral Juan Manuel <juanmanuelcabralll@gmail.com>
2024-11-18 21:15:32 -0300

    Corregir importaciones

--- backend/routes/clients.js
+++ backend/routes/clients.js
@@ -1,51 +1,44 @@
 import express from "express"
-import Client from "../model/clients.js"
+import Client from "../models/client.js"
 
 const router = express.Router()
-
 
 // Obtenemos todos los clientes
 router.get("/", async (req, res) => {
   try {
-    const clients = await Client.find();
-    res.json(clients);
+    const clients = await Client.find()
+    res.json(clients)
   } catch (error) {
     res
       .status(500)
-      .json({ error: "Error al obtener los clientes: " + error.message });
+      .json({ error: "Error al obtener los clientes: " + error.message })
   }
-});
+})
 
 // Crear un nuevo cliente
-router.post("/", async(req, res) => {
-  const {
-  name,
-  lastname,
-  email,
-  phone,
-  } = req.body
+router.post("/", async (req, res) => {
+  const { name, lastname, email, phone } = req.body
 
-  
-  try{
-    const e_mail = await Client.findOne({email})
+  try {
+    const e_mail = await Client.findOne({ email })
 
-    if(e_mail){
-      return res.status(400).json({error: "Email ya regustrado"})
+    if (e_mail) {
+      return res.status(400).json({ error: "Email ya regustrado" })
     }
 
     const client = new Client({
       name,
       lastname,
       email,
-      phone,
+      phone
     })
-    await client.save();
+    await client.save()
 
-    res.status(201).json({ message: "Cliente creado con éxito", client });
+    res.status(201).json({ message: "Cliente creado con éxito", client })
   } catch (error) {
     res
       .status(500)
-      .json({ error: "Error al crear el cliente: " + error.message });
+      .json({ error: "Error al crear el cliente: " + error.message })
   }
 })
 

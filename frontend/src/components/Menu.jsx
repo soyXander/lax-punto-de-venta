387b9a4a723e99c4b597a2265224ef81676ba28f
Marcos G Leotta <leottaneonatologia@gmail.com>
2024-11-19 14:03:30 -0300

    Mejorar Barra Lateral del menu

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,13 +1,13 @@
 const Menu = () => {
   return (
-    <nav>
-      <ul>
-        <li><a href="/">Inicio</a></li>
-        <li><a href="/pos">POS</a></li>
-        <li><a href="/productos">Productos</a></li>
-        <li><a href="/ventas">Ventas</a></li>
-        <li><a href="/usuarios">Usuarios</a></li>
-        <li><a href="/clientes">Clientes</a></li>
+    <nav  className="bg-sky-700  text-white p-20 rounded-lg shadow px-10 ">
+<ul className="flex flex-wrap gap-20">
+        <li><a href="/"className="border rounded-lg px-20 py-5 bg-orange-500 text-white hover:bg-blue-900 transition duration-300">Inicio</a></li>
+        <li><a href="/pos"className="border rounded-lg px-20 py-5 bg-orange-500 text-white hover:bg-blue-900 transition duration-300">POS</a></li>
+        <li><a href="/productos"className=" border rounded-lg px-20 py-5 bg-orange-500 text-white  hover:bg-blue-900 transition duration-300">Productos</a></li>
+        <li><a href="/ventas"className="border rounded-lg px-20 py-5 bg-orange-500 text-white rounded-lg shadow hover:bg-blue-900 transition duration-300">Ventas</a></li>
+        <li><a href="/usuarios"className="border rounded-lg px-20 py-5 bg-orange-500 text-white rounded-lg shadow hover:bg-blue-900 transition duration-300">Usuarios</a></li>
+        <li><a href="/clientes"className="border rounded-lg px-20 py-5 bg-orange-500 text-white hover:bg-blue-900 transition duration-300">Clientes</a></li>
       </ul>
     </nav>
   )

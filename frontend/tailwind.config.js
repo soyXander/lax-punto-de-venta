bcae16f5ce7fc8463fe655b19bb901fef51aca3a
Candela Zapata <candezapata2011@hotmail.com>
2024-11-18 23:37:08 -0300

    Corregir paleta de colores

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,3 @@
-import { info } from 'autoprefixer';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,14 +5,15 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      neutral: '#001524',
-      base: '#FFECD1',
-      primary: '#ff7b00',
-      secundary:'#75290f',
-      accent: '#14606c',
+    extend: {
+      colors: {
+        neutral: '#001524',
+        base1: '#FFECD1',
+        primary: '#ff7b00',
+        secundary:'#75290f',
+        accent: '#14606c'
+      },
     },
-    extend: {},
   },
   plugins: [],
 }

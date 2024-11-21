c3cf8de791f1626a9e3e7c8a968459cbc86299aa
guemesfb <guemesfb@gmail.com>
2024-11-21 17:45:37 -0300

    formato con prettier para el modelo de producto

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 
 const productSchema = new mongoose.Schema({
-    name : {type:String, required:true},
-    description: {type:String},
-    barcode: { type:String, required:true},
-    category_id : {type:String},
-    price : {type:String, required:true},
-    stock : {type:Int32()},
-    createdAt : {type:Date, default:Date.now},
-    updatedAt : {type:Date}
+  name: { type: String, required: true },
+  description: { type: String },
+  barcode: { type: String, required: true },
+  category_id: { type: String },
+  price: { type: String, required: true },
+  stock: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date }
 })
 
-export default mongoose.model('Product', productSchema)
+export default mongoose.model("Product", productSchema)

aa462d44d63bcceae798a9124bac5bedf1d8e310
Xander.- <zn.xander@gmail.com>
2024-11-21 04:04:45 -0300

    Mas mejoras visuales para el POS

--- frontend/src/pages/POS.jsx
+++ frontend/src/pages/POS.jsx
@@ -1,82 +1,85 @@
+import "./POS.css"
+
 const POS = () => {
   const productos = [
     {
       nombre: "Producto 1",
       precio: 10,
-      cantidad: 3,
+      cantidad: 3
     },
     {
       nombre: "Producto 2",
       precio: 20,
-      cantidad: 1,
+      cantidad: 1
     },
     {
       nombre: "Producto 3",
       precio: 30,
-      cantidad: 5,
+      cantidad: 5
     },
     {
       nombre: "Producto 4",
       precio: 40,
-      cantidad: 2,
+      cantidad: 2
     },
     {
       nombre: "Producto 5",
       precio: 50,
-      cantidad: 4,
+      cantidad: 4
     },
     {
       nombre: "Producto 6",
       precio: 60,
-      cantidad: 3,
+      cantidad: 3
     },
     {
       nombre: "Producto 7",
       precio: 70,
-      cantidad: 2,
+      cantidad: 2
     },
     {
       nombre: "Producto 8",
       precio: 80,
-      cantidad: 1,
+      cantidad: 1
     },
     {
       nombre: "Producto 9",
       precio: 90,
-      cantidad: 5,
-    },
+      cantidad: 5
+    }
   ]
 
   const categorias = [
     {
-      nombre: "Panaderia",
+      nombre: "Panaderia"
     },
     {
-      nombre: "Gaseosas",
+      nombre: "Gaseosas"
     },
     {
-      nombre: "Fiambres",
+      nombre: "Fiambres"
     },
     {
-      nombre: "Carnes",
+      nombre: "Carnes"
     },
     {
-      nombre: "Frutas",
-    },
+      nombre: "Frutas"
+    }
   ]
 
   return (
-    <div className="flex flex-col min-h-dvh bg-base">
-      <header className="h-12 flex items-center shrink-0">
+    <div className="flex min-h-dvh flex-col bg-base">
+      <header className="flex h-12 shrink-0 items-center">
         <div className="">
-          <button className="text-3xl font-bold text-white bg-primary bg-opacity-70 hover:bg-opacity-100 duration-300 p-2 rounded-md m-1">
+          <button className="m-1 rounded-md bg-primary bg-opacity-70 p-2 text-3xl font-bold text-white duration-300 hover:bg-opacity-100">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="w-6 h-6">
+              className="h-6 w-6"
+            >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -86,51 +89,59 @@ const POS = () => {
           </button>
         </div>
       </header>
-      <main className="flex flex-row justify-center gap-x-3 flex-grow px-8">
-        <div className="basis-3/4 shadow">
-          <div className="flex justify-between py-2">
-            <button className="bg-secondary bg-opacity-60 hover:bg-opacity-100 duration-300 text-white font-semibold rounded-lg p-2">
+      <main className="flex flex-grow flex-row justify-center gap-x-3 px-8">
+        <div className="basis-3/4 rounded-lg bg-white shadow">
+          <div className="flex justify-between p-2">
+            <button className="rounded-lg bg-secondary bg-opacity-60 p-2 font-semibold text-white duration-300 hover:bg-opacity-100">
               + AGREGAR PRODUCTO NUEVO
             </button>
-            <div className="flex justify-end items-center">
+            <div className="flex w-2/4 items-center justify-end">
               <input
                 type="text"
                 placeholder="Buscar producto..."
-                className="bg-secondary bg-opacity-20 border-2 border-transparent hover:border-primary focus:border-primary focus:outline-none duration-300 text-neutral rounded-full p-2"
+                className="w-full rounded-full border-2 border-transparent bg-secondary bg-opacity-20 p-2 text-center text-neutral duration-300 hover:border-primary focus:border-primary focus:outline-none"
               />
               <a href="">🔎</a>
             </div>
           </div>
-          <div className="flex flex-wrap overflow-y-scroll h-[calc(100vh-13rem)] bg-white justify-evenly gap-y-3 py-3">
+          <div className="flex h-[calc(100vh-13rem)] flex-wrap justify-evenly gap-y-3 overflow-y-scroll bg-gray-200 py-3">
             {productos.map((producto, index) => (
               <article
-                className="flex flex-col max-w-[320px] shadow-md hover:shadow-2xl duration-500 border-4 border-transparent hover:border-primary rounded-lg p-3 justify-center items-center bg-secondary bg-opacity-50 text-neutral"
-                key={index}>
+                className="flex max-w-[320px] flex-col items-center justify-center rounded-lg border-4 border-transparent bg-secondary bg-opacity-50 p-3 text-neutral shadow-md duration-500 hover:border-primary hover:shadow-2xl"
+                key={index}
+              >
                 <img
                   className="mb-3 rounded-lg"
                   src="https://www.shutterstock.com/shutterstock/photos/2232098881/display_1500/stock-photo-different-fresh-vegetables-for-eating-healthy-fresh-vegetables-in-basket-isolated-on-white-2232098881.jpg"
                   alt=""
                 />
-                <h2 className="font-bold">{producto.nombre}</h2>
-                <p>${producto.precio.toFixed(2)}</p>
+                <div className="flex w-full flex-col items-start justify-start">
+                  <h2 className="font-bold">{producto.nombre}</h2>
+                  <p>
+                    ${producto.precio.toFixed(2)}{" "}
+                    <span className="text-xs font-bold text-accent">
+                      Stock: {producto.cantidad}
+                    </span>
+                  </p>
+                </div>
               </article>
             ))}
           </div>
 
-          <div className="flex h-12 ">
+          <div className="flex h-12">
             {categorias.map((categoria, index) => (
               <button
-                className="basis-1/5 flex flex-col bg-primary bg-opacity-70 hover:bg-opacity-100 border rounded-lg p-3 m-1 justify-center items-center text-neutral 
-                font-semibold shadow-md shadow-transparent hover:shadow-gray-500 duration-300"
-                key={index}>
+                className="m-1 flex basis-1/5 flex-col items-center justify-center rounded-lg border bg-primary bg-opacity-70 p-3 font-semibold text-neutral shadow-md shadow-transparent duration-300 hover:bg-opacity-100 hover:shadow-gray-500"
+                key={index}
+              >
                 {categoria.nombre}
               </button>
             ))}
           </div>
         </div>
-        <div className="basis-1/4 flex flex-col flex-grow justify-between shadow bg-white">
+        <div className="flex flex-grow basis-1/4 flex-col justify-between bg-white shadow">
           <div className="flex flex-col">
-            <h2 className="font-bold my-4 text-neutral">Checkout</h2>
+            <h2 className="my-4 font-bold text-neutral">Checkout</h2>
             <table className="table-fixed border">
               <thead className="bg-gray-100">
                 <tr className="border border-gray-200 text-neutral">
@@ -143,14 +154,15 @@ const POS = () => {
                 {productos.map((producto, index) => (
                   <tr
                     key={index}
-                    className="border border-gray-200 text-neutral">
+                    className="border border-gray-200 text-neutral"
+                  >
                     <td>{producto.nombre}</td>
                     <td>
-                      <button className="h-6 w-6 bg-primary opacity-70 hover:opacity-100 rounded-full mx-2 text-neutral font-bold">
+                      <button className="mx-2 h-6 w-6 rounded-full bg-primary font-bold text-neutral opacity-70 hover:opacity-100">
                         -
                       </button>
                       {producto.cantidad}
-                      <button className="h-6 w-6 bg-primary opacity-70 hover:opacity-100 rounded-full mx-2 text-neutral font-bold">
+                      <button className="mx-2 h-6 w-6 rounded-full bg-primary font-bold text-neutral opacity-70 hover:opacity-100">
                         +
                       </button>
                     </td>
@@ -160,15 +172,16 @@ const POS = () => {
               </tbody>
             </table>
           </div>
-          <ul className="text-neutral p-4 bg-gray-200 border-gray-200">
+          <ul className="border-gray-200 bg-gray-200 p-4 text-neutral">
             <li className="flex justify-between">
               <span>Descuento:</span>
               <span>
                 $
                 <input
-                  className="text-right w-20"
+                  className="w-20 text-right"
                   type="number"
-                  placeholder="0.00"></input>
+                  placeholder="0.00"
+                ></input>
               </span>
             </li>
             <li className="flex justify-between">
@@ -186,17 +199,17 @@ const POS = () => {
           </ul>
         </div>
       </main>
-      <footer className="h-12 flex flex-shrink-0 gap-x-3 px-8">
-        <div className="basis-3/4 flex justify-end gap-x-3">
-          <button className="px-8 bg-secondary bg-opacity-70 hover:bg-opacity-100 transition-all duration-300 border rounded-lg text-white font-semibold h-full shadow-md shadow-transparent hover:shadow-gray-500">
+      <footer className="flex h-12 flex-shrink-0 gap-x-3 px-8 py-1">
+        <div className="flex basis-3/4 justify-end gap-x-3">
+          <button className="h-full rounded-lg border bg-secondary bg-opacity-70 px-8 font-semibold text-white shadow-md shadow-transparent transition-all duration-300 hover:bg-opacity-100 hover:shadow-gray-500">
             Cancelar venta
           </button>
-          <button className="px-8 bg-primary bg-opacity-70 hover:bg-opacity-100 transition-all duration-300 border rounded-lg text-neutral font-semibold h-full shadow-md shadow-transparent hover:shadow-gray-500">
+          <button className="h-full rounded-lg border bg-primary bg-opacity-70 px-8 font-semibold text-neutral shadow-md shadow-transparent transition-all duration-300 hover:bg-opacity-100 hover:shadow-gray-500">
             Mantener venta
           </button>
         </div>
         <div className="basis-1/4">
-          <button className="px-8 bg-primary bg-opacity-70 hover:bg-opacity-100 transition-all duration-300 border rounded-lg text-neutral font-semibold w-full h-full shadow-md shadow-transparent hover:shadow-gray-500 uppercase">
+          <button className="h-full w-full rounded-lg border bg-primary bg-opacity-70 px-8 font-semibold uppercase text-neutral shadow-md shadow-transparent transition-all duration-300 hover:bg-opacity-100 hover:shadow-gray-500">
             Finalizar venta
           </button>
         </div>

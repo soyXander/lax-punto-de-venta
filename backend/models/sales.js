d549d016da6ef37f6cc373588276c66d12d74ad2
Salas Cabral Juan Manuel <juanmanuelcabralll@gmail.com>
2024-11-25 21:20:20 -0300

    Crear lista de productos

--- backend/models/sales.js
+++ backend/models/sales.js
@@ -1,4 +1,11 @@
 import mongoose from "mongoose"
-const salesSchema = new mongoose.Schema({})
+const salesSchema = new mongoose.Schema({
+  products: [
+    {
+      product_id: { type: String, require: true },
+      quantity: { type: String, require: true }
+    }
+  ]
+})
 
 export default mongoose.model("salesSchema", salesSchema)

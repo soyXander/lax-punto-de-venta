e8754364cc6ffa9c0bd6b88f19fa1e5da8fa1df6
Salas Cabral Juan Manuel <juanmanuelcabralll@gmail.com>
2024-11-25 23:16:24 -0300

    Crear endpoint Get

--- backend/routes/sales.js
+++ backend/routes/sales.js
@@ -1,6 +1,19 @@
-import express, { Router } from "express"
+import express from "express"
 import Sale from "../models/sale.js"
+import Product from "../models/product.js"
 
 const router = express.Router()
+
+//Obtener lista de ventas
+router.get("/", async (req, res) => {
+  try {
+    const sales = await Sale.find()
+    res.json(sales)
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "La lista de ventas no se encuentra" + error.message })
+  }
+})
 
 export default router

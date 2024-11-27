032d044d9fecae29d956587c54d415dadc534482
Xander.- <zn.xander@gmail.com>
2024-11-26 22:29:47 -0300

    Instalar DaisyUI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,19 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        neutral: '#001524',
-        base: '#ffecd1',
-        primary: '#ff7b00',
-        secondary:'#75290f',
-        accent: '#14606c'
-      },
-    },
+    extend: {}
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#001524",
+          secondary: "#75290f",
+          accent: "#14606c",
+          neutral: "#001524",
+          base: "#ffecd1"
+          // info: "#0000ff",
+          // success: "#00ff00",
+          // warning: "#00ff00",
+          // error: "#ff0000"
+        }
+      }
+    ]
+  }
 }

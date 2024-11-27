583102ed5388b0d38c744db0396235dd57fe8455
Xander.- <zn.xander@gmail.com>
2024-11-26 22:30:09 -0300

    Cambiar layout

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,23 +1,19 @@
-import Menu from '../components/Menu'
-import Logo from "../components/Logo"
+import Menu from "../components/Menu"
+import Header from "../components/Header"
 
 const Layout = (props) => {
-
   return (
-    <div className='min-h-dvh w-full flex'>
-      <div className='w-1/6'>
-        <Logo />
-        <Menu />
+    <>
+      <Header />
+      <div className="flex min-h-dvh w-full">
+        <div className="w-1/6">
+          <Menu />
+        </div>
+        <div className="w-full bg-gray-100">
+          <main>{props.component}</main>
+        </div>
       </div>
-      <div className='w-full bg-gray-100'>
-        <header>
-          <h1>Header</h1>
-        </header>
-        <main>
-          {props.component}
-        </main>
-      </div>
-    </div>
+    </>
   )
 }
 

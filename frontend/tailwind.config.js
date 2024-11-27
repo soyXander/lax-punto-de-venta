eb1fa60a21f678a886d1c3edc39796e4469ecd2c
Xander.- <zn.xander@gmail.com>
2024-11-27 06:15:28 -0300

    Corregir color base en frontend

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -14,13 +14,14 @@ export default {
           secondary: "#75290f",
           accent: "#14606c",
           neutral: "#001524",
-          base: "#ffecd1"
+          "base-100": "#ffecd1"
           // info: "#0000ff",
           // success: "#00ff00",
           // warning: "#00ff00",
           // error: "#ff0000"
         }
       }
-    ]
+    ],
+    logs: false
   }
 }

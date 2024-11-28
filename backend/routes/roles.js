ebf212cf3455e4e7b80cc3d2fff7e6afadd7ad42
Xander.- <zn.xander@gmail.com>
2024-11-28 12:06:08 -0300

    Rutas para Roles

---
+++ backend/routes/roles.js
@@ -0,0 +1,19 @@
+import express from "express"
+import Role from "../models/role.js"
+import { validateRole, validateToken } from "../middlewares/auth.js"
+
+const router = express.Router()
+
+router.get("/", validateToken, validateRole(["admin"]), async (req, res) => {
+  try {
+    const roles = await Role.find()
+    res.json(roles)
+  } catch (error) {
+    console.error(error)
+    res
+      .status(500)
+      .json({ error: "Error al obtener los roles: " + error.message })
+  }
+})
+
+export default router

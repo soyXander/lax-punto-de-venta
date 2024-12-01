d7c178a707212f160b3f8259773a78b59e4b9ae6
Xander.- <zn.xander@gmail.com>
2024-12-01 18:14:32 -0300

    Ahora la contraseña de usuarios no es parte de las consultas

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   lastName: { type: String, required: true },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   email: { type: String, required: true, unique: true },
   roleId: {
     type: mongoose.Schema.Types.ObjectId,
